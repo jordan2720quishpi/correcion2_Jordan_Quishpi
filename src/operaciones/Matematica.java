bd4bab61a96893f7db921bba216d741f0b943dee
jordan quishpi <jiquishp1@espe.edu.ec>
2021-06-02 10:42:22 -0500

    v1 clase Matematica

---
+++ src/operaciones/Matematica.java
@@ -0,0 +1,12 @@
+package operaciones;
+
+public class Matematica {
+
+    int a;
+    int b;
+
+    public void sumar() {
+        int resultado = a + b;
+        System.out.println("El resultado es" + resultado);
+    }
+}
---
+++ src/operaciones/PruebaMatematica.java
@@ -0,0 +1,12 @@
+
+package operaciones;
+
+public class PruebaMatematica {
+    public static void main(String [] args){
+        Matematica ob1 = new Matematica();
+        ob1.a=25;
+        ob1.b=35;
+        ob1.sumar();
+    }
+    
+}
